b78ce48ea4bd431dc0c2859c16726a67af5e824f
Fahmi, Shayan <Shayan_Fahmi@Dell.com>
2022-09-21 23:14:00 +0530

    Producer Changes

--- ProducerConsumerRabbitMQ/Producer/Sender.cs
+++ ProducerConsumerRabbitMQ/Producer/Sender.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
 
 namespace Producer
 {
@@ -6,7 +8,21 @@ namespace Producer
     {
         public static void Main(string[] args)
         {
+            var factory = new ConnectionFactory() { HostName = "localhost" }; // Create a factory
+            using (var connection = factory.CreateConnection()) //Open a connection
+            using (var channel = connection.CreateModel()) //Open a channel
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null); //Declare the queue
 
+                string message = "Getting started with .Net Core RabbitMQ";
+                var body = Encoding.UTF8.GetBytes(message); //Create a message
+                
+                channel.BasicPublish("", "BasicTest", null, body); //Publish a message
+                Console.WriteLine("Sent message {0}...", message);
+            }
+
+            Console.WriteLine("Press [enter] to exit the Sender App...");
+            Console.ReadLine();
         }
     }
 }
